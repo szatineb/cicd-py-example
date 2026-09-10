a7a18b7239a5ad1c7ba40c3833f8b73bdeef9b76
szatineb <sz.atineb@gmail.com>
2026-09-10 20:06:52 +0200

    Update test_mymath.py

--- test_mymath.py
+++ test_mymath.py
@@ -6,7 +6,7 @@ def test_add_positive():
     assert add_numbers(1, 2) == 3
 
 def test_add_zero():
-    assert add_numbers(1, 0) == 1
+    assert add_numbers(1, 0) == 100
 
 def test_add_negative():
     assert add_numbers(4, -100) == -96
